6b0b4da9ab67ddd5589103310f6eebd8632f3cb4
j082175 <j082175@gmail.com>
2022-02-14 11:27:20 +0900

    uploaded Test02_1

--- 열혈C++예제연습/Test02_1.cpp
+++ 열혈C++예제연습/Test02_1.cpp
@@ -1,1 +1,44 @@
 #include "Test02_1.h"
+
+ Test02_1::Q1::Q1(const int& value)
+	:ref(value)
+{}
+
+ void Test02_1::Q1::Print()
+{
+	upper(ref);
+	std::cout << "upper : " << ref << std::endl;
+	changeMark(ref);
+	std::cout << "changeMakr : " << ref << std::endl;
+}
+
+ void Test02_1::Q1::upper(int& ref) const
+{
+	ref++;
+}
+
+ void Test02_1::Q1::changeMark(int& ref) const
+{
+	ref = -ref * 2;
+}
+
+ Test02_1::Q3::Q3(const int& num1, const int& num2)
+	:num1(num1),
+	num2(num2)
+{}
+
+ void Test02_1::Q3::Print()
+{
+	int numOne = num1;
+	int numTwo = num2;
+	std::cout << numOne << " " << numTwo << std::endl;
+	swapPointer(&numOne, &numTwo);
+	std::cout << numOne << " " << numTwo << std::endl;
+}
+
+ void Test02_1::Q3::swapPointer(int* ptr1, int* ptr2)
+{
+	int data = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = data;
+}
--- 열혈C++예제연습/Test02_1.h
+++ 열혈C++예제연습/Test02_1.h
@@ -7,26 +7,12 @@ namespace Test02_1
 	class Q1
 	{
 	public:
-		Q1(const int& value = 0)
-			:ref(value)
-		{}
-		void Print()
-		{
-			upper(ref);
-			std::cout << "upper : " << ref << std::endl;
-			changeMark(ref);
-			std::cout << "changeMakr : " << ref << std::endl;
-		}
+		Q1(const int& value = 0);
+		void Print();
 	private:
-		void upper(int& ref)const
-		{
-			ref++;
-		}
+		void upper(int& ref)const;
 
-		void changeMark(int& ref)const
-		{
-			ref =- ref * 2;
-		}
+		void changeMark(int& ref)const;
 
 	private:
 		int ref;
@@ -35,26 +21,11 @@ namespace Test02_1
 	class Q3
 	{
 	public:
-		Q3(const int& num1, const int& num2)
-			:num1(num1),
-			num2(num2)
-		{}
+		Q3(const int& num1, const int& num2);
 
-		void Print()
-		{
-			int numOne = num1;
-			int numTwo = num2;
-			std::cout << numOne << " " << numTwo << std::endl;
-			swapPointer(&numOne, &numTwo);
-			std::cout << numOne << " " << numTwo << std::endl;
-		}
+		void Print();
 	private:
-		void swapPointer(int* ptr1, int* ptr2)
-		{
-			int data = *ptr1;
-			*ptr1 = *ptr2;
-			*ptr2 = data;
-		}
+		void swapPointer(int* ptr1, int* ptr2);
 
 	private:
 		int num1;
